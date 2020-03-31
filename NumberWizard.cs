a4509ca5aa31b164222d8cf5b1b87522f36d9424
Zach Bridges <56745560+bridgeszach@users.noreply.github.com>
2020-03-31 14:43:49 -0400

    Add files via upload

---
+++ NumberWizard.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Global Variables
+    int maxGuess;
+    int minGuess;
+    int guess;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        maxGuess = 1000;
+        minGuess = 1;
+        guess = 500;
+
+        Debug.Log("Welcome to the Number Wizard");
+        Debug.Log("Please pick a number between " + minGuess + " and " + maxGuess);
+        Debug.Log("Tell me if your number is higher or lower than: " + guess);
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        maxGuess = maxGuess + 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Detect when the up arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {           
+            minGuess = guess;
+            NextGuess();
+            
+        }
+
+        //Detect when the down arrow key is pressed down
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            maxGuess = guess;
+            NextGuess();
+        }
+
+        //Detect when the Return key is pressed down
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("I am a genius!");
+            StartGame();
+        }
+
+    }
+
+    void NextGuess()
+    {
+        guess = (maxGuess + minGuess) / 2;
+        Debug.Log("Is it higher or lower than: " + guess);
+    }
+}
